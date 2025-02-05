cf49286fa02cae31a867fe11f262ee38e77e9f2f
thegabriew <raimundogabrielpf@gmail.com>
2025-02-05 09:41:09 -0300

    Corrigindo erros de sintaxe e organização

--- Models/admnistrator.js
+++ Models/admnistrator.js
@@ -1,74 +1,76 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
-
+const { Model } = require('sequelize');
 const bcrypt = require('bcrypt');
 
 module.exports = (sequelize, DataTypes) => {
-  class Admnistrator extends Model {
+  class Administrator extends Model {
     static associate(models) {
-           Admnistrator.hasMany(models.Course, {
-        foreignKey: 'admin_id', 
-        as: 'courses' 
+      Administrator.hasMany(models.Course, {
+        foreignKey: 'admin_id',
+        as: 'courses'
       });
     }
   }
-  Admnistrator.init({
-    id: {
-      type: DataTypes.UUID,
-      primaryKey: true,
-      autoIncrement: true,
-      unique: true,
-      defaultValue: DataTypes.UUIDV4,
-      allowNull: false
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        isAlphanumeric: {
-          args: true,
-          msg: "O nome deve ser alfanumérico."
-        },
-        len: {
-          args: [3, 100],
-          msg: "O nome deve ter entre 3 e 100 caracteres."
+
+  Administrator.init(
+    {
+      id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        unique: true,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isAlphanumeric: {
+            args: true,
+            msg: "O nome deve ser alfanumérico."
+          },
+          len: {
+            args: [3, 100],
+            msg: "O nome deve ter entre 3 e 100 caracteres."
+          }
         }
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: {
+            args: [8, 100],
+            msg: "A senha deve ter entre 8 e 100 caracteres."
+          }
+        }
+      },
+      created_at: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+        allowNull: false
+      },
+      updated_at: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+        allowNull: false
       }
     },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: {
-          args: [8, 100],
-          msg: "A senha deve ter entre 8 e 100 caracteres."
+    {
+      sequelize,
+      modelName: 'Administrator',
+      hooks: {
+        beforeCreate: async (administrator) => {
+          if (administrator.password) {
+            const salt = await bcrypt.genSalt(10);
+            administrator.password = await bcrypt.hash(administrator.password, salt);
+          }
         }
-      }
-    },
-    created_at: {
-      type: DataTypes.DATE,
-      defaultValue: DataTypes.NOW,
-      allowNull: false
-    },
-    updated_at: {
-      type: DataTypes.DATE,
-      defaultValue: DataTypes.NOW,
-      allowNull: false
+      },
+      underscored: true
     }
-  }, {
-    sequelize,
-    modelName: 'Admnistrator',
-    hooks: { 
-      beforeCreate: async (admnistrator) => {
-        if (admnistrator.password) {
-          const salt = await bcrypt.genSalt(10);
-          admnistrator.password = await bcrypt.hash(admnistrator.password, salt);
-        }
-      }
-    }
-  });
-  return Admnistrator;
+  );
+
+  return Administrator;
 };
