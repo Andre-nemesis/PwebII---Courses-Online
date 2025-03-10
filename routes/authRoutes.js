fa432d454342ca5503c6e228a536a377267ff928
Geovanna <geovanna.lima60@aluno.ifce.edu.br>
2025-03-10 08:56:17 -0300

    fix: signUp route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,10 @@
 import express from 'express';
-//import { login, logout, createAccount } from '../Controllers/authController.js';
+import { login, logout, signUp } from '../Controllers/authController.js';
 
 const router = express.Router();
 
 router.post('/login', login);
 router.post('/logout', logout);
-router.post('/createAccount', createAccount);
+router.post('/createAccount', signUp);
 
 export default router;
