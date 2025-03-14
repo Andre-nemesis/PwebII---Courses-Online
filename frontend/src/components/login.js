c80584e650413734e021fa85287f252cc7e97858
Geovanna <geovanna.lima60@aluno.ifce.edu.br>
2025-03-13 22:35:50 -0300

    add: page for login and route

---
+++ frontend/src/components/login.js
@@ -0,0 +1,93 @@
+import React, { useState } from "react";
+import { login } from '../service/auth';
+import { TextField, Container, Button, Typography, Paper, Box, CircularProgress } from '@mui/material';
+import { Email, Lock } from '@mui/icons-material'; 
+
+const Login = ({ onLogin }) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false); 
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setLoading(true);
+        setError(null);
+
+        try {
+            await login(email, password);
+            onLogin();
+        
+        } catch (err) {
+            setError('E-mail ou senha inválida!');
+        
+        } finally {
+            setLoading(false);
+        } 
+    };
+
+    const isEmailValid = () => {
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+    };
+
+    return (
+        <Container component="main" maxWidth="xs">
+        <Paper elevation={3} sx={{ mt: 8, p: 4, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          <Typography component="h1" variant="h5" sx={{ mb: 2 }}>
+            Login
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} sx={{ width: '100%' }}>
+            {/* Campo de Email */}
+            <TextField
+              fullWidth
+              margin="normal"
+              id="email"
+              label="Email"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              error={!!email && !isEmailValid()} 
+              helperText={!!email && !isEmailValid() ? 'Email inválido' : ''}
+              InputProps={{
+                startAdornment: <Email sx={{ color: 'action.active', mr: 1 }} />, // Ícone de email
+              }}
+            />
+
+            {/* Campo de Senha */}
+            <TextField
+              fullWidth
+              margin="normal"
+              id="password"
+              label="Senha"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              InputProps={{
+                startAdornment: <Lock sx={{ color: 'action.active', mr: 1 }} />, // Ícone de senha
+              }}
+            />
+
+            {/* Exibição de Erro */}
+            {error && (
+              <Typography color="error" variant="body2" sx={{ mt: 1 }}>
+                {error}
+              </Typography>
+            )}
+
+            {/* Botão de Login */}
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              disabled={loading || !isEmailValid() || !password} // Desabilita o botão se estiver carregando ou os campos forem inválidos
+              sx={{ mt: 3, mb: 2, bgcolor: 'primary.main', '&:hover': { bgcolor: 'primary.dark' } }}
+            >
+              {loading ? <CircularProgress size={24} /> : 'Entrar'} {/* Ícone de carregamento */}
+            </Button>
+          </Box>
+        </Paper>
+      </Container>
+    );
+}
+
+export default Login;
--- frontend/src/routes/Routes.js
+++ frontend/src/routes/Routes.js
@@ -2,13 +2,31 @@ import React from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
 import { SignUpAdmin } from '../components/signUp/signUpAdmin.js';
 import { Hello } from '../components/HelloReact';
+import MainScreen from '../components/mainScreen.js';
+import Login from '../components/login.js';
 
 
-  const AppRoutes = () => {
+  const AppRoutes = ({ isAuthenticated, setAuthenticated }) => {
+    const handleLogin = () => {
+      setAuthenticated(true);
+    };
+
     return (
       <Routes>
         <Route path="/signUp" element={<SignUpAdmin />} />
         <Route path="/Hello" element={<Hello />} />
+        <Route path="/login" element={!isAuthenticated ? <Login onLogin={handleLogin} /> : <Navigate to="/mainScreen" />} />
+        <Route path="/mainScreen" element={<MainScreen />} />
+
+        <Route path="/*"
+          element = {
+            isAuthenticated ? (
+              <MainScreen setAuthenticated = {setAuthenticated} />
+            ) : (
+              <Navigate to="/login" />
+            )
+          }
+        />
       </Routes>
     );
   };
