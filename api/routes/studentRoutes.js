2a4d49ba8fe4111b9c5cbc4c396388eb33a07fc0
Nickolas <nickolasdavi209@gmail.com>
2025-03-15 14:08:14 -0300

    add: routes to student

--- api/routes/studentRoutes.js
+++ api/routes/studentRoutes.js
@@ -10,6 +10,13 @@ router.get('/certificates/view/:id');
 router.get('/courses/view');
 router.get('/courses/view/:id');
 router.get('/courses/view/:id/modules');
+router.get('/viewStudent/:id');
+router.get('/viewTeacher/:id');
+router.get('/viewCompleteCourse');
+router.get('/searchCourse/:id');
+
+// put
+router.put('/editPassword/:id');
 
 //delete
 router.delete('/account/:id');
